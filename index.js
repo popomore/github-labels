8a4c1ad7b0b84f8a9f69cb85204d8ebed172318f
Raman Gupta <rocketraman@gmail.com>
2022-02-24 15:24:01 -0500

    Fix auth
    Use token-based auth only

--- index.js
+++ index.js
@@ -3,38 +3,27 @@
 const fs = require('fs');
 const path = require('path');
 const co = require('co');
-const auth = require('./lib/auth');
 const label = require('./lib/label');
 const color = require('./lib/colors.json');
 const dotfile = getDotFile();
-const GitHubApi = require('github');
+const { Octokit } = require('@octokit/rest');
 
 module.exports = program => {
-  const github = new GitHubApi({
-    version: '3.0.0',
-    protocol: 'https',
-    pathPrefix: program.pathPrefix,
-    host: program.host,
-  });
   co(function* () {
-    let token = program.token || readToken();
+    const token = program.token || readToken();
+
+    const octokit = new Octokit({
+      auth: token,
+    });
+
+    const repo = program.args[0].split('/');
 
     const opt = {
       config: parse(program.config),
-      token,
-      repo: program.args[0],
-      github,
+      owner: repo[0],
+      repo: repo[1],
+      github: octokit.rest,
     };
-
-    /*
-      Github Authorization
-    */
-
-    token = yield auth(opt);
-    if (token) {
-      yield saveToken(token);
-    }
-    console.info('>> Authorized');
 
     /*
       Force option will delete add existing labels
@@ -82,12 +71,6 @@ function readToken() {
     return fs.readFileSync(dotfile, 'utf8').replace(/\n$/, '');
   }
   return null;
-}
-
-function saveToken(token) {
-  return function(callback) {
-    fs.writeFile(dotfile, token, callback);
-  };
 }
 
 function randomColor() {
